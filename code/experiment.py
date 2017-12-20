08106737ec682d165c49d143ffb342aee797d1d5
Cathy Chen <cathychen2357@gmail.com>
2017-12-20 12:29:19 -0500

    Add result writing and results.

--- code/experiment.py
+++ code/experiment.py
@@ -7,7 +7,6 @@ Created on Mon Dec 18 14:26:03 2017
 from sklearn.datasets import fetch_lfw_people
 #from sklearn.model_selection import train_test_split
 
-#import pickle as p
 import experiment_utils as utils
 import numpy as np
 
@@ -29,9 +28,8 @@ def split_traintest(targets):
 
     return train_indices, test_indices
 
-def get_lfw_dataset():
+def get_lfw_dataset(min_faces_per_person):
     """ Return train and test data and labels from 'Labeled Faces in the Wild" dataset."""
-    min_faces_per_person = 20
     dataset = fetch_lfw_people(min_faces_per_person=min_faces_per_person)
     data = dataset.data # num_people x image_length
     mean_face = np.mean(data, axis=0)
@@ -56,27 +54,35 @@ def compute_accuracy(predictions, targets):
 def run_experiment(train_function, predict_function, model_name):
     """ Trains and tests using train_model_function and evaluate_model_function
     arguments, and saves results. """
-    train_data, train_targets, test_data, test_targets = get_lfw_dataset()
+    min_faces_per_person = 20
+    train_data, train_targets, test_data, test_targets = get_lfw_dataset(min_faces_per_person)
     
     model = train_function(train_data, train_targets)
     train_predictions = predict_function(model, train_data)
     train_accuracy = compute_accuracy(train_predictions, train_targets)
     test_predictions = predict_function(model, test_data)
     test_accuracy = compute_accuracy(test_predictions, test_targets)
-
+    
+    # Print results.
     num_faces = len(np.unique(train_targets))
     print("Recognition Algorithm: %s" % model_name)
     print("Number of distinct faces: %d" % num_faces)
     print("Chance rate: %f" % (1 / num_faces))
-    print("Results for %s Algorithm" % model_name)
     print("Train accuracy: %f" % train_accuracy)
     print("Test accuracy: %f" % test_accuracy)
+    print("\n")
     
-#    save_dict = {"model":model,"train_data":train_data,"train_targets":train_targets,"test_data":test_data,"test_targets":test_targets}
-#    p.dump(save_dict, open( "../results/%s_results.p" % model_name, "wb" ))
-
+    # Save results.
+    with open("../results/%s_results.txt" % (model_name).replace(" ",""), "a") as f:
+        f.write("Min faces per person: %d\n" % min_faces_per_person)
+        f.write("Number of distinct faces: %d\n" % num_faces)
+        f.write("Chance rate: %f\n" % (1 / num_faces))
+        f.write("Train accuracy: %f\n" % train_accuracy)
+        f.write("Test accuracy: %f\n" % test_accuracy)
+        f.write("\n\n")
+        
 if __name__ == "__main__":
-    #run_experiment(utils.train_pca, utils.predict_pca, "PCA")
-    #run_experiment(utils.train_sparserepresentation, utils.predict_sparserepresentation, "Sparse Representation")
-    run_experiment(utils.train_sparserepresentation_dimension_reduction, utils.predict_sparserepresentation_dimension_reduction, "Sparse Representation, Dimension Reduction")
-    run_experiment(utils.train_sparserepresentation_combinedl1, utils.predict_sparserepresentation_combinedl1, "Sparse Representation, Combined l1")
+    run_experiment(utils.train_pca, utils.predict_pca, "PCA")
+    run_experiment(utils.train_sparserepresentation, utils.predict_sparserepresentation, "Sparse Representation")
+    run_experiment(utils.train_sparserepresentation_dimension_reduction, utils.predict_sparserepresentation_dimension_reduction, "Sparse Representation Dimension Reduction")
+    run_experiment(utils.train_sparserepresentation_combinedl1, utils.predict_sparserepresentation_combinedl1, "Sparse Representation Combined l1")
